ea3bed380a2f2f21a5b980333977776528ce7fe7
Alastair Coote <alastair.coote@nytimes.com>
2025-06-20 15:54:19 -0400

    simplify

--- MessageHandlersTest/ViewController.swift
+++ MessageHandlersTest/ViewController.swift
@@ -1,6 +1,37 @@
 import UIKit
 import WebKit
 
+let dummyHTML = """
+  <!doctype>
+  <html>
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </head>
+    <body>
+      <p>
+        <button id='same-domain'>Add same-domain iframe</button>
+      </p>
+      <p>
+        <button id='cross-domain'>Add cross-domain iframe</button>
+      </p>
+      <p>
+        <a href="https://different.example/two.html">Navigate and evaluate JS</a>
+      <p>
+      <script>
+        function addIframe(domain) {
+          const el = document.createElement("iframe");
+          el.src = `https://${domain}/iframe.html`;
+          document.body.appendChild(el);
+        }
+
+        document.getElementById('same-domain').addEventListener('click', () => addIframe('test.example'));
+        document.getElementById('cross-domain').addEventListener('click', () => addIframe('another.example'));
+
+      </script>
+    </body>
+  </html>
+"""
+
 class ViewController: UIViewController, WKNavigationDelegate {
   let configuration: WKWebViewConfiguration = {
     let configuration = WKWebViewConfiguration()
@@ -9,68 +40,55 @@ class ViewController: UIViewController, WKNavigationDelegate {
     return configuration
   }()
 
-  let messageHandler = MessageHandler()
-  let urlRequest: URLRequest
   lazy var webView = WKWebView(frame: .zero, configuration: configuration)
 
-  init(urlRequest: URLRequest) {
-    self.urlRequest = urlRequest
+  init() {
     super.init(nibName: nil, bundle: nil)
   }
   
   required init?(coder: NSCoder) {
-    self.urlRequest = URLRequest(url: URL(string: "https://www.nytimes.com?psw=true&isPreloaded=true")!)
     super.init(coder: coder)
   }
   
   override func viewDidLoad() {
     super.viewDidLoad()
     webView.navigationDelegate = self
-    webView.configuration.userContentController.add(messageHandler, name: "Foo")
-    webView.load(urlRequest)
+    webView.loadHTMLString(dummyHTML, baseURL: URL(string: "https://test.example/one.html")!)
   }
 
   override func loadView() {
     view = webView
     webView.isInspectable = true
   }
-
-  func webView(
-    _ webView: WKWebView,
-    decidePolicyFor navigationAction: WKNavigationAction
-  ) async -> WKNavigationActionPolicy {
-    let url = navigationAction.request.url!
-
-//    let hostsToBlock = Set([
-//      "googlesyndication.com",
-//      "google.com",
-//      "adtrafficquality.google"
-//    ])
-//
-//    for host in hostsToBlock {
-//      if url.host()?.hasSuffix(host) == true {
-//        print("blocking navigation:", url)
-//        return .cancel
-//      }
-//    }
-
-    print("navigation:", navigationAction.navigationType.rawValue, url)
-
+  
+  func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping @MainActor (WKNavigationActionPolicy) -> Void) {
+    
     guard navigationAction.navigationType == .linkActivated else {
-      return .allow
+      decisionHandler(.allow)
+      return
     }
+    print("Cancelling navigation")
+    decisionHandler(.cancel)
+    
+    self.webView.evaluateJavaScript("""
+      (function() {
+        const p = document.createElement('p');
+        p.innerHTML = "Successfully called JS from app.";
+        document.body.appendChild(p);
+      })();
+      1+2
 
-    navigationController?.pushViewController(
-      ViewController(urlRequest: navigationAction.request),
-      animated: true
-    )
-
-    return .cancel
+    """, in: nil,in: WKContentWorld.defaultClient) { result in
+      switch result {
+      case .success(let value):
+        assert(value as? Int == 3)
+        print("Successfully evaluated JavaScript")
+      case .failure(let err):
+        fatalError(err.localizedDescription)
+      }
+      
+    }
+    
   }
-}
 
-final class MessageHandler: NSObject, WKScriptMessageHandler {
-  func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-    print("Received message", message.body)
-  }
 }
